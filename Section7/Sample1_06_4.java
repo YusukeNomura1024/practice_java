5cd5c10a532062dab9d6b81e1dd57cb9d1560059
YusukeNomura1024 <joblesser@gmail.com>
2022-02-12 23:46:50 +0900

    [Finish] 論理演算

--- Section7/Sample1_06_4.java
+++ Section7/Sample1_06_4.java
@@ -7,19 +7,20 @@ class Sample1_06_4 {
 		int numberY = 4 ;
 		int numberZ = 5 ;
 		
-		//『 && 』
+		//『 && 』 両辺がtrueの時true
 		boolean logicalValue1 = numberX != numberY && numberY > numberZ ;
+		//上記の場合　logigalValue = true && falseとなるのでfalseとなる
 		System.out.println("logicalValue1：" + logicalValue1);
 		
-		//『 || 』
+		//『 || 』どちらかがtureであればtrueになる、左辺がtrueであれば、右辺は見に行かない
 		boolean logicalValue2 = numberX != numberY || numberY > numberZ ;
 		System.out.println("logicalValue2：" + logicalValue2);
 		
-		//『 ^ 』
+		//『 ^ 』　左辺と右辺が違っていれば、true
 		boolean logicalValue3 = numberX != numberY ^ numberY > numberZ ;
 		System.out.println("logicalValue3：" + logicalValue3);
 		
-		// 反転
+		// 反転 boolean型の前に！を付けるとtrueとfalseが反転する
 		boolean logicalValue4 = !logicalValue3 ;
 		System.out.println("logicalValue4：" + logicalValue4);
 		
