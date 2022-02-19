408f224c7d5b76390dc3733caf55f961c391a036
YusukeNomura1024 <joblesser@gmail.com>
2022-02-20 00:41:31 +0900

    [Finish] 演習問題

--- sec15_演習問題/zihan2.java
+++ sec15_演習問題/zihan2.java
@@ -1,11 +1,12 @@
-public class zihan1 {
+public class zihan2 {
 	public static void main (String[] args) {
     final String ERRMESSAGE_1 =        "「警告：１円玉は使えません。」"; //メッセージ（A型）
     final String ERRMESSAGE_5 =        "「警告：５円玉は使えません。」"; //メッセージ（B型）
     final String ERRMESSAGE_DEFAULT =        "「警告：入力された値は効果として適切ではありません。」"; //メッセージ（O型）
 
     int totalCountMoney = 0;
-    for(int i = 0; i < args.length; i++){
+    int productCost = Integer.parseInt(args[args.length -1]);
+    for(int i = 0; i < args.length-1; i++){
       int entryMoney = Integer.parseInt(args[i]);
       if(entryMoney == 10 || entryMoney == 50 || entryMoney == 100 || entryMoney == 500){
         totalCountMoney += entryMoney;
@@ -23,6 +24,10 @@ public class zihan1 {
         }
       }
     }
-    System.out.println("ただいまの投入金額は" + totalCountMoney + "円です");
+    // System.out.println("ただいまの投入金額は" + totalCountMoney + "円です");
+    // System.out.println("商品金額は" + productCost + "円です");
+    int change = totalCountMoney - productCost;
+    System.out.println( change + "円のお釣りです。ありがとうございました。");
+
 	}
 }
