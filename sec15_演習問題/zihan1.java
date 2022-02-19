c91fb1a31bce3388860e126c303c28393ed72663
YusukeNomura1024 <joblesser@gmail.com>
2022-02-19 23:52:38 +0900

    [Start] 演習問題

---
+++ sec15_演習問題/zihan1.java
@@ -0,0 +1,28 @@
+public class Ex1_13_2 {
+	public static void main (String[] args) {
+    final String MESSAGE_A =        "あなたは几帳面な性格の方ですね？"; //メッセージ（A型）
+    final String MESSAGE_B =        "あなたはマイペースな性格の方ですね？"; //メッセージ（B型）
+    final String MESSAGE_O =        "あなたは大雑把な性格ですね？"; //メッセージ（O型）
+    final String MESSAGE_AB =       "あなたは天才肌な方ですね？"; //メッセージ（AB型）
+    final String MESSAGE_DEFAULT =  "数字の1～4で答えてください"; //メッセージ（その他）
+
+		int bloodNum = Integer.parseInt(args[0]);
+		switch(bloodNum) {
+      case 1:
+        System.out.println("A" + MESSAGE_A);
+        break;
+      case 2:
+        System.out.println("B" + MESSAGE_B);
+        break;
+      case 3:
+        System.out.println("O" + MESSAGE_O);
+        break;
+      case 4:
+        System.out.println("AB" + MESSAGE_AB);
+        break;
+      default:
+        System.out.println("エラー" + MESSAGE_DEFAULT);
+        break;
+    }
+	}
+}
