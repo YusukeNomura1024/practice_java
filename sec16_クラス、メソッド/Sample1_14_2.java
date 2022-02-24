af555509c6e54df1e2bd473f867596fc70f60cd9
YusukeNomura1024 <joblesser@gmail.com>
2022-02-24 22:59:30 +0900

    [Finish] Sample1_14_2

--- sec16_クラス、メソッド/Sample1_14_2.java
+++ sec16_クラス、メソッド/Sample1_14_2.java
@@ -8,12 +8,17 @@ class Sample1_14_2 {
 	}
 	
 	//戻り値なしのメソッド
+	//戻り値がない場合は型名を書かずにvoidとする
+	//型がvoidの場合はreturnを使うとエラーになってしまう
 	static void printName(String name) {
 		
 		System.out.println("なまえ：" + name);
 		
+		
 	}
 	
+	//mainメソッドはプログラムが起動されて必ず一番初めに実行される特別なメソッドです。
+	//仮にmainメソッドがクラス内にない場合は処理を開始することができないので、エラーになります。
 	//プログラムは必ずmainメソッドからはじまる
 	public static void main(String[] args) {
 		
