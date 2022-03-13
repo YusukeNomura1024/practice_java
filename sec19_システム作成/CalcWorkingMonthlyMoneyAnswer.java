0eb3047b63b401a2c063d58b89fbf31cb4476907
YusukeNomura1024 <joblesser@gmail.com>
2022-03-13 22:31:51 +0900

    [Fix] システム作成

--- sec19_システム作成/CalcWorkingMonthlyMoneyAnswer.java
+++ sec19_システム作成/CalcWorkingMonthlyMoneyAnswer.java
@@ -7,7 +7,7 @@ import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CalcWorkingMonthlyMoney {
+public class CalcWorkingMonthlyMoneyAnswer {
 
 	public static void main(String[] args) {
 		//  WorkingResult.csvのパス ※「C:\WorkSpace」直下に配置していない場合は適宜変更してください。
@@ -17,19 +17,7 @@ public class CalcWorkingMonthlyMoney {
 		final String COMMA = ","; 
 
 		// 計算用の数値を定数で用意
-		final long ONE_HOUR_BY_MILLI_SEC = 1000 * 60 * 60; // 1時間のミリ秒換算
-		final long ONE_MIN_BY_MILLI_SEC  = 1000 * 60;      // 1分のミリ秒換算
-		final int  ONE_HOUR_BY_MIN       = 60;   					 // 1時間の分換算
-		final int  HOURLY_WAGE					 = 900;            // 時給
-		int  minuteWage					 = HOURLY_WAGE / 60;            // 1分当たりの給料
-		
-		
-		
-		final Long BORDER_OVERTIME			 = 28800000L;			//残業ライン
-		final double  RATE_OVERTIME			 = 1.25;            // 残業倍率
-
 		List<String> workingResults = new ArrayList<String>(); //ファイルから読み込んだデータの格納用
-
 		//  WorkingResult.csvを読み込む
 		try {
 			// WorkingResult.csvの読み込み準備
@@ -59,50 +47,50 @@ public class CalcWorkingMonthlyMoney {
 			// getTimeメソッドを使って労働時間をミリ秒（0.001秒単位）で取得する
 			// ※getTime()メソッドの戻り値はlong型であることに注意
 			// long workingTime = finishTime.getTime() - startTime.getTime();
-			long workingTime = workingTimeCalc(startTime, finishTime);
-			Long restTime = restTimeCalc(workingTime);
-
-			int restMin = (int)(restTime / ONE_MIN_BY_MILLI_SEC);
-			Long overWorkingTime = 0L;
-			workingTime -= restTime;
-			if(workingTime > BORDER_OVERTIME){
-				overWorkingTime = workingTime - BORDER_OVERTIME;
-				workingTime = BORDER_OVERTIME;
-			}
-		
-			// ミリ秒で取得した労働時間を○時間△分の形式に直す
-			int workingHour = (int)( workingTime / ONE_HOUR_BY_MILLI_SEC );                      // 時間に換算
-			int workingMin  = (int)( ( workingTime / ONE_MIN_BY_MILLI_SEC ) % ONE_HOUR_BY_MIN ); // 分に換算
-			int todayWorkingWage = (int)(workingHour * HOURLY_WAGE + workingMin * HOURLY_WAGE / 60);
-
-			int workingHourOverTime = (int)( overWorkingTime / ONE_HOUR_BY_MILLI_SEC );
-			int workingMinOverTime  = (int)( ( overWorkingTime / ONE_MIN_BY_MILLI_SEC ) % ONE_HOUR_BY_MIN ); // 分に換算
-			int todayWorkingOverTimeWage = (int)(workingHourOverTime * HOURLY_WAGE * RATE_OVERTIME + workingMinOverTime * HOURLY_WAGE * RATE_OVERTIME / 60);
-
-
-			// 出力
-			System.out.println("-----------" + workingDate + "-----------");
-			System.out.println(
-				"定時内労働時間" + workingHour + "時間" + workingMin + "分 " +
-				"休憩時間は" + restMin + "分 " +
-				"残業時間は" + workingHourOverTime + "時間" + workingMinOverTime + "分"
-			);
-			System.out.println(
-				"定時労働分の給料" + todayWorkingWage + "円 " +
-				"残業分の給料" + todayWorkingOverTimeWage + "円"
-			);
-
-			monthlyMoney = monthlyMoney + todayWorkingWage + todayWorkingOverTimeWage;
+			monthlyMoney = monthlyMoney + dailyMoney(workingDate, startTime, finishTime);
 		}
 		System.out.println("-----------給料日-----------");
 		System.out.println("今月の給料は" + monthlyMoney + "円です。");
+	}
+	public static int dailyMoney(Data wdate, Time st, Time fin){
+		long workingTime = workingTimeCalc(st, fin);
+		Long restTime = restTimeCalc(workingTime);
+		final Long BORDER_OVERTIME			 = 28800000L;			//残業ライン
+		final long ONE_HOUR_BY_MILLI_SEC = 1000 * 60 * 60; // 1時間のミリ秒換算
+		final long ONE_MIN_BY_MILLI_SEC  = 1000 * 60;      // 1分のミリ秒換算
+		final int  ONE_HOUR_BY_MIN       = 60;   					 // 1時間の分換算
+		final int  HOURLY_WAGE					 = 900;            // 時給
+		final double  RATE_OVERTIME			 = 1.25;            // 残業倍率
+		int  minuteWage					 = HOURLY_WAGE / 60;            // 1分当たりの給料
+		int restMin = (int)(restTime / ONE_MIN_BY_MILLI_SEC);
+		Long overWorkingTime = 0L;
+		workingTime -= restTime;
+		if(workingTime > BORDER_OVERTIME){
+			overWorkingTime = workingTime - BORDER_OVERTIME;
+			workingTime = BORDER_OVERTIME;
+		}
+				// ミリ秒で取得した労働時間を○時間△分の形式に直す
+		int workingHour = (int)( workingTime / ONE_HOUR_BY_MILLI_SEC );                      // 時間に換算
+		int workingMin  = (int)( ( workingTime / ONE_MIN_BY_MILLI_SEC ) % ONE_HOUR_BY_MIN ); // 分に換算
+		int todayWorkingWage = (int)(workingHour * HOURLY_WAGE + workingMin * HOURLY_WAGE / 60);
 
+		int workingHourOverTime = (int)( overWorkingTime / ONE_HOUR_BY_MILLI_SEC );
+		int workingMinOverTime  = (int)( ( overWorkingTime / ONE_MIN_BY_MILLI_SEC ) % ONE_HOUR_BY_MIN ); // 分に換算
+		int todayWorkingOverTimeWage = (int)(workingHourOverTime * HOURLY_WAGE * RATE_OVERTIME + workingMinOverTime * HOURLY_WAGE * RATE_OVERTIME / 60);
 
-		// // バイトの開始時間と終了時間をコマンドライン引数から受け取る
-		// Time startTime  = Time.valueOf(args[0]);
-		// Time finishTime = Time.valueOf(args[1]);
+		// 出力
+		System.out.println("-----------" + wdate + "-----------");
+		System.out.println(
+			"定時内労働時間" + workingHour + "時間" + workingMin + "分 " +
+			"休憩時間は" + restMin + "分 " +
+			"残業時間は" + workingHourOverTime + "時間" + workingMinOverTime + "分"
+		);
+		System.out.println(
+			"定時労働分の給料" + todayWorkingWage + "円 " +
+			"残業分の給料" + todayWorkingOverTimeWage + "円"
+		);
 
-
+		return todayWorkingWage + todayWorkingOverTimeWage;
 	}
 
 	public static Long workingTimeCalc(Time st, Time fin ){
