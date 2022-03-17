b7073968c445c91ad6c822a8d060507ecf10008b
YusukeNomura1024 <joblesser@gmail.com>
2022-03-18 00:38:00 +0900

    [Finish] コレクション 拡張for

--- 応用/03_コレクション/ExpansionForTest.java
+++ 応用/03_コレクション/ExpansionForTest.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class ExpansionForTest{
     public static void main(String[] args){
         List<String> prefectures = new ArrayList<>();
@@ -10,6 +14,23 @@ public class ExpansionForTest{
         prefectures.add("秋田県");
         for(String prefecture : prefectures) {
             System.out.println(prefecture);
+        }
+
+        Map<Integer, String> classmates = new HashMap<>();
+        classmates.put(1, "青木");
+        classmates.put(2, "石坂");
+        classmates.put(3, "小野田");
+        for(Integer key : classmates.keySet()){
+            System.out.println(key);
+        }
+
+        for(String name : classmates.values()){
+            System.out.println(name);
+        }
+
+        for(Map.Entry<Integer, String> classmate : classmates.entrySet()){
+            System.out.println(classmate.getKey() + "番は" + classmate.getValue());
+
         }
     }
 }
--- 応用/03_コレクション/ListTest.java
+++ 応用/03_コレクション/ListTest.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+
+
 public class ListTest {
     public static void main(String args[]){
         List<String> list1 = new ArrayList<String>();
