84f1bd10c4839633011ed97af8ce11fab19a5412
YusukeNomura1024 <joblesser@gmail.com>
2022-04-06 21:12:38 +0900

    [Fix] Sample2_02_2_car.java

--- Object/04_コンストラクター/Sample2_02_2_car.java
+++ Object/04_コンストラクター/Sample2_02_2_car.java
@@ -1,65 +1,73 @@
 class Sample2_02_2_car {
-	
-	//---フィールド（クラス直下で定義された変数）----------------
-	
-	String  carModel ;  //車種名
-	String  owner    ;  //オーナー
-	String  color    ;  //塗装色
-	int     speed    ;  //現在の速度
-	boolean right    ;  //ライト（true:点灯/false:消灯）
-	
-	//---コンストラクタ------------------------------------------------
-	
-	//コンストラクタ①（引数なし）
-	Sample2_02_2_car(){
-		System.out.println("　　▼コンストラクタ①（引数なし）-------------------------") ;
-		carModel = "未登録" ;  //車種名
-		owner    = "未登録" ;  //オーナー
-		color    = "未登録" ;  //塗装色
-		speed    = 0        ;  //現在の速度
-		right    = false    ;  //ライト（true:点灯/false:消灯）
-		System.out.println("　　▲-----------------------------------------------------") ;
+
+	// ---フィールド（クラス直下で定義された変数）----------------
+
+	String carModel; // 車種名
+	String owner; // オーナー
+	String color; // 塗装色
+	int speed; // 現在の速度
+	boolean right; // ライト（true:点灯/false:消灯）
+
+	// ---コンストラクタ------------------------------------------------
+
+	// コンストラクタ①（引数なし）
+	Sample2_02_2_car() {
+		System.out.println("　　▼コンストラクタ①（引数なし）-------------------------");
+		carModel = "未登録"; // 車種名
+		owner = "未登録"; // オーナー
+		color = "未登録"; // 塗装色
+		speed = 0; // 現在の速度
+		right = false; // ライト（true:点灯/false:消灯）
+		System.out.println("　　▲-----------------------------------------------------");
 	}
-	
-	//コンストラクタ②（引数あり）
-	Sample2_02_2_car(String carModel , String owner , String color , int speed , boolean right ){
-		this();                    //コンストラクタ①（引数なし）の起動
-		System.out.println("　　▼コンストラクタ②（引数あり）-------------------------") ;
-		this.carModel = carModel ;  //車種名
-		owner         = owner    ;  //オーナー
-		color         = color    ;  //塗装色
-		speed         = speed    ;  //現在の速度
-		right         = right    ;  //ライト（true:点灯/false:消灯）
-		System.out.println("　　▲-----------------------------------------------------") ;
+
+	// コンストラクタ②（引数あり）
+	Sample2_02_2_car(String cm, String on, String cl, int sp, boolean rt) {
+		this(); // コンストラクタ①（引数なし）の起動
+		// コンストラクターを呼び出すときは内の処理の中で最初に行わにといけないので、２行目以降にthis()と記述するとエラーになる
+		System.out.println("　　▼コンストラクタ②（引数あり）-------------------------");
+		this.carModel = cm; // 車種名
+		// this.ownerを使わずにowner= ownerとしてしまうと左のownerはローカル変数の方になってしまうので、
+		// ローカル変数にローカル変数を入れているだけになるので、処理が終了すると全く意味がなくなってしまう。
+		// だからthis.ownerでフィールド変数を呼び出してきてローカル変数の値を代入する必要がある
+		// thisを付け忘れるという事故も起こりうるから、ローカル変数とフィールド変数はかぶせないように心がけよう
+		// フィールド変数とローカル変数がこのようにかぶることを避けるときは、ローカル変数側は省略した名前にしても理解できるので、
+		// この場合のローカル変数はcarModelを単にcmに省略してもOk
+		this.owner = on; // オーナー
+		this.color = cl; // 塗装色
+		this.speed = sp; // 現在の速度
+		this.right = rt; // ライト（true:点灯/false:消灯）
+		System.out.println("　　▲-----------------------------------------------------");
 	}
-	
-	//---メソッド------------------------------------------------
-	
-	//acceleratorメソッド（現在の速度を1km/h上げる）
-	void accelerator(){
-		speed++ ;
-		System.out.println(owner + "の車：（少し速くなった）") ;
+
+	// ---メソッド------------------------------------------------
+
+	// acceleratorメソッド（現在の速度を1km/h上げる）
+	void accelerator() {
+		speed++;
+		System.out.println(owner + "の車：（少し速くなった）");
 	}
-	
-	//brakeメソッド（現在の速度を1km/h下げる）
-	void brake(){
-		speed-- ;
-		System.out.println(owner + "の車：（少し遅くなった）") ;
+
+	// brakeメソッド（現在の速度を1km/h下げる）
+	void brake() {
+		speed--;
+		System.out.println(owner + "の車：（少し遅くなった）");
 	}
-	
-	//rightOnメソッド（ライトを点灯させる）
-	void rightOn(){
-		right = true ;
-		System.out.println(owner + "の車：（周囲が明るくなった）") ;
+
+	// rightOnメソッド（ライトを点灯させる）
+	void rightOn() {
+		right = true;
+		System.out.println(owner + "の車：（周囲が明るくなった）");
 	}
-	
-	//rightOffメソッド（ライトを消灯する）
-	void rightOff(){
-		right = false ;
-		System.out.println(owner + "の車：（周囲が暗くなった）") ;
+
+	// rightOffメソッド（ライトを消灯する）
+	void rightOff() {
+		right = false;
+		System.out.println(owner + "の車：（周囲が暗くなった）");
 	}
-	//klaxonメソッド（クラクションを鳴らす）
-	void klaxon(){
-		System.out.println(owner + "の車：「プップ～～～～～～～～～～ッ」") ;
+
+	// klaxonメソッド（クラクションを鳴らす）
+	void klaxon() {
+		System.out.println(owner + "の車：「プップ～～～～～～～～～～ッ」");
 	}
 }
