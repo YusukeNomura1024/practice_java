288ded3e273c0f4685615ce8d504aac3bbc39ad5
YusukeNomura1024 <joblesser@gmail.com>
2022-04-12 01:17:30 +0900

    [Finish] 継承2

---
+++ Object/06_継承/Sample2_05_2_FireTruck.java
@@ -0,0 +1,33 @@
+import sample.Sample2_05_2_Car;
+
+class Sample2_05_2_FireTruck extends Sample2_05_2_Car {
+	
+	//---フィールド------------------------------------------------------
+	//※なし
+	
+	//---コンストラクタ--------------------------------------------------
+	//※なし
+	
+	//---メソッド--------------------------------------------------------
+	
+	//paintメソッド（色を塗る）※セットできる塗装色は「RED」のみ
+	public void paint( String cl ){
+		if( cl.equals( "RED" ) ){
+			this.color = cl ;
+		}
+	}
+	
+	//オーバーライドをしている
+	//オーバーライドの条件はスーパークラスの戻り値、引数と数が同じであること、アクセス制限が同じか緩いこと
+	//acceleratorメソッド（現在の速度を1km/h上げる）※80km/h以上にはならないよう制御
+	public void accelerator(){
+		if( this.getSpeed() < 80 ){
+			this.setSpeed(this.getSpeed() + 1);
+		}
+	}
+
+	public void subbrake(){
+		super.brake();
+	}
+	
+}
